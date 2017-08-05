e9af47350d2d5331308ecc23a905faf598ce7e8c
adamZakar <30754828+adamZakar@users.noreply.github.com>
2017-08-05 23:38:26 +0200

    Add files via upload

--- flexionTest/src/test/java/com/flexionmobile/codingchallenge/integration/CodingChallengeIntegrationTest.java
+++ flexionTest/src/test/java/com/flexionmobile/codingchallenge/integration/CodingChallengeIntegrationTest.java
@@ -14,38 +14,38 @@ public class CodingChallengeIntegrationTest {
     IntegrationTestRunner integrationTestRunner = new IntegrationTestRunner();
     CodingChallengeIntegration codingChallengeIntegration = new CodingChallengeIntegration();
 
-    ExpectedException expectedException = ExpectedException.none();
-
-    private final int numberOfRepetitions = 10;
-
-    @Before
-    public void setUpBeforeTest() {
-        for (int i = 0; i < numberOfRepetitions; i++) {
-            codingChallengeIntegration.buy("item1");
-        }
-    }
-
-    @Test
-    public void testPurchases() {
-        integrationTestRunner.runTests(codingChallengeIntegration);// for the original test, the rest should be commented out
-        assertEquals(codingChallengeIntegration.getPurchases().size(), equals(numberOfRepetitions));
-        codingChallengeIntegration.consume(codingChallengeIntegration.getPurchases().get(0));
-        assertTrue(codingChallengeIntegration.getPurchases().get(0).getConsumed());
-        assertFalse(codingChallengeIntegration.getPurchases().get(1).getConsumed());
-    }
-
-    @Test
-    public void testPurchasesExceptions() {
-        expectedException.expect(NullPointerException.class);
-        expectedException.expectMessage("There were no server response");
-        codingChallengeIntegration.buy("drhtd");
-
-    }
-
-    @After
-    public void reset() {
-        //here should be written the reset method, but it was not included into the pdf.
-    }
+//    ExpectedException expectedException = ExpectedException.none();
+//
+//    private final int numberOfRepetitions = 10;
+//
+//    @Before
+//    public void setUpBeforeTest() {
+//        for (int i = 0; i < numberOfRepetitions; i++) {
+//            codingChallengeIntegration.buy("item1");
+//        }
+//    }
+//
+//    @Test
+//    public void testPurchases() {
+//        integrationTestRunner.runTests(codingChallengeIntegration);// for the original test, the rest should be commented out
+//        assertEquals(codingChallengeIntegration.getPurchases().size(), equals(numberOfRepetitions));
+//        codingChallengeIntegration.consume(codingChallengeIntegration.getPurchases().get(0));
+//        assertTrue(codingChallengeIntegration.getPurchases().get(0).getConsumed());
+//        assertFalse(codingChallengeIntegration.getPurchases().get(1).getConsumed());
+//    }
+//
+//    @Test
+//    public void testPurchasesExceptions() {
+//        expectedException.expect(NullPointerException.class);
+//        expectedException.expectMessage("There were no server response");
+//        codingChallengeIntegration.buy("drhtd");
+//
+//    }
+//
+//    @After
+//    public void reset() {
+//        //here should be written the reset method, but it was not included into the pdf.
+//    }
 
     @Test
     public void originalTestCase() {
